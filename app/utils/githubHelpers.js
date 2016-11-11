588ef69a659e3b3744fa7bb4164e5c7b1c67191f
Mario Suharja <me@mario.sg>
2016-11-12 00:29:05 +0700

    Concise Object Methods & Arrow Functions
    - Use concise object methods (make anonymous function declaration shorter (without function word))
    - Use arrow functions

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -14,20 +14,18 @@ function getRepos (username) {
 }
 
 function getTotalStars (repos) {
-  return repos.data.reduce(function (prev, current) {
-    return prev + current.stargazers_count;
-  }, 0);
+  return repos.data.reduce((prev, current) => prev + current.stargazers_count, 0);
 }
 
 function getPlayersData (player) {
   return getRepos(player.login)
     .then(getTotalStars)
-    .then(function (totalStars) {
-      return {
+    .then((totalStars) => (
+      {
         followers: player.followers,
         totalStars: totalStars
-      };
-    });
+      }
+    ));
 }
 
 function calculateScores (players) {
@@ -38,18 +36,14 @@ function calculateScores (players) {
 }
 
 export function getPlayersInfo (players) {
-  return axios.all(players.map(function (username) {
-    return getUserInfo(username);
-  })).then(function (info) {
-    return info.map(function (user) {
-      return user.data;
-    });
-  }).catch(function (error) {
-    return logCustomMessage(error.response.statusText, {
+  return axios.all(players.map((username) => getUserInfo(username)))
+    .then((info) => info.map((user) => user.data))
+    .catch((error) => (
+    logCustomMessage(error.response.statusText, {
       players: players,
       error: error
-    });
-  });
+    })
+  ));
 };
 
 export function battle (playersInfo) {
@@ -58,10 +52,10 @@ export function battle (playersInfo) {
 
   return axios.all([playerOneData, playerTwoData])
     .then(calculateScores)
-    .catch(function (error) {
-      return logCustomMessage(error.response.statusText, {
+    .catch((error) => (
+      logCustomMessage(error.response.statusText, {
         players: [playerOneData, playerTwoData],
         error: error
-      });
-    });
+      })
+    ));
 };
